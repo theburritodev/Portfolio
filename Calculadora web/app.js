c02292ffef184cafee6418901804c2ef14f28797
Leonardo Pinezi <88010163+theburritodev@users.noreply.github.com>
2024-05-14 22:32:43 -0300

    Update app.js

--- Calculadora web/app.js
+++ Calculadora web/app.js
@@ -1,1 +1,8 @@
-
+const numkeys = document.querySelectorAll(".numPad");
+for (var i = 0; i < numkeys.length; i++) {
+    let key = numkeys[i];
+    
+  key.addEventListener("click",(e)=>{
+    document.getElementById("display").textContent += key.innerText;
+  });
+}
