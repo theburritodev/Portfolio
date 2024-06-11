54ac20174758328702c0965bbdf122244a5940b6
Leonardo Pinezi <88010163+theburritodev@users.noreply.github.com>
2024-06-10 21:54:59 -0300

    Update script.js

---
+++ postit-online/script.js
@@ -0,0 +1,48 @@
+const apiUrl = 'https://sheetdb.io/api/v1/3c5algt76yp88';
+
+function post()
+{
+  const novoPostIt = {
+        content: document.getElementById("postit").innerText,
+        id: Date.now()
+      };
+  const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(novoPostIt)
+};
+  fetch(apiUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      alert('Novo post-it adicionado: ' + novoPostIt.id);
+    })
+    .catch(error => {
+      console.error('Erro ao adicionar novo post-it:', error);
+    });
+
+}
+
+function obterDadosDoSheetDB() {
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  fetch(apiUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("postit").innerText = data[0].content;
+    })
+    .catch(error => {
+      console.error('Erro ao obter dados da base de dados:', error);
+    });
+}
+
+document.getElementById("name").addEventListener("change",(e)=>{
+  obterDadosDoSheetDB();
+});
